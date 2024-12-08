85d807597bd97ed2a14b6b56a1fef1e69ac8ae0a
Mikhail Band <misha_banda06@mail.ru>
2024-12-08 23:13:21 +0300

    Осталось разобраться со скиллы и фидбэком

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(".menu-element__link").click(function() {
+    const thisElement = $(this);
+    if (thisElement.hasClass("active")){
+        return;
+    }
+
+    const linkHref = thisElement.attr("href");
+    const hrefElement = $(linkHref);
+
+    const paddingTop= parseInt(hrefElement.css("padding-top"));
+    const menuHeight = $(".header-top").height();
+    $([document.documentElement, document.body]).animate({
+        scrollTop: hrefElement.offset().top-paddingTop-menuHeight
+    }, 1000);
+
+
+
+    $(".menu-element__link.active").removeClass("active");
+    thisElement.addClass('active');
+
+
+
+});
