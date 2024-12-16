b7944cd7fc9de415af6bb10fe8395db256ed2ff2
Mikhail Band <misha_banda06@mail.ru>
2024-12-16 23:29:12 +0300

    Пока все

--- js/main.js
+++ js/main.js
@@ -1,64 +1,10 @@
-$('.contact-me__form').on('submit', function (event) {
-
-    event.stopPropagation();
-    event.preventDefault();
-
-    let form = this,
-    submit = $('.submit', form),
-    data = new FormData(),
-    files = $('input[type=file]')
-
-
-    $('.submit', form).val('Отправка...');
-    $('input, textarea', form).attr('disabled','');
-
-    data.append( 'name', 		$('[name="name"]', form).val() );
-    data.append( 'email', 		$('[name="email"]', form).val() );
-    data.append( 'message', 	$('[name="message"]', form).val() );
-
-
-    files.each(function (key, file) {
-        let cont = file.files;
-        if ( cont ) {
-            $.each( cont, function( key, value ) {
-                data.append( key, value );
-            });
-        }
+emailjs.init("0i2CquX_SBb2cuOcJEzkr"); // Этот ID будет вашим виртуальным почтальоном
+document.getElementById('contact-me__form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Не обновляйте страницу, пока сообщение не отправлено
+    emailjs.sendForm('your_service_id', 'your_template_id', this)
+        .then(function() {
+        alert('Сообщение успешно отправлено!');
+    }, function(error) {
+        alert('Ошибка отправки: ' + error);
     });
-
-    $.ajax({
-        url: 'ajax.php',
-        type: 'POST',
-        data: data,
-        cache: false,
-        dataType: 'json',
-        processData: false,
-        contentType: false,
-        xhr: function() {
-            let myXhr = $.ajaxSettings.xhr();
-
-            if ( myXhr.upload ) {
-                myXhr.upload.addEventListener( 'progress', function(e) {
-                    if ( e.lengthComputable ) {
-                        let percentage = ( e.loaded / e.total ) * 100;
-                        percentage = percentage.toFixed(0);
-                        $('.submit', form)
-                            .html( percentage + '%' );
-                    }
-                }, false );
-            }
-
-            return myXhr;
-        },
-        error: function( jqXHR, textStatus ) {
-            // Тут выводим ошибку
-        },
-        complete: function() {
-            // Тут можем что-то делать ПОСЛЕ успешной отправки формы
-            console.log('Complete')
-            form.reset()
-        }
-    });
-
-    return false;
 });
